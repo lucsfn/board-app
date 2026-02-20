418753bc336d7a7be4083ddead28b7b0b35eff23
Lucas Fernandes <lucasfernandes1506@gmail.com>
2026-02-20 11:39:10 -0300

    feat: implement kanban board home page with backlog section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,27 @@
+import { ArchiveIcon } from "lucide-react";
+import { Section } from "@/components/section";
+
 export default function Home() {
   return (
-    <main>
-      <div>Hello world!</div>
-    </main>
+    <div className="max-w-405 w-full mx-auto p-10 flex flex-col gap-8 h-dvh">
+      <div />
+      <main className="grid grid-cols-4 gap-5 flex-1 items-stretch">
+        <Section.Root>
+          <Section.Header>
+            <Section.Title>
+              <ArchiveIcon className="size-3" />
+              Backlog
+            </Section.Title>
+            <Section.IssueCount>16</Section.IssueCount>
+          </Section.Header>
+
+          <Section.Content>
+            <div>Card 1</div>
+            <div>Card 2</div>
+            <div>Card 3</div>
+          </Section.Content>
+        </Section.Root>
+      </main>
+    </div>
   );
 }
